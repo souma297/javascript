bca7a8202d4ed8223df340d761fdd652337aaa7b
souma297 <soumyalal2002@gmail.com>
2024-09-24 14:37:04 +0000

    opertor

---
+++ soumya/function.js
@@ -0,0 +1,7 @@
+console.log( 2==2) //eqal to- true
+console.log( 3!=3) //not eqal to false
+console.log( 2==='2')// strictly eqal to - false
+console.log( 2!=='2')//strctly not eqal yo - true
+console.log( 2 >2)// greater than - false
+console.log( 3 >=3)//greatr than or eqal to - true
+console.log( 2 <=2)//less than or eqal to -true
